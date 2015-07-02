f9d0d952344fc4b4a24b895f7f0ed1508a2c7e88
Fatjon Hushi <me@fatjoni.com>
2015-07-02 15:02:39 +0200

    Implemented all suggestions made by my reviewer on udacity on this link: https://review.udacity.com/#!/reviews/24293

--- app/src/main/java/com/fatjoni/droid/myportfolio/MainActivity.java
+++ app/src/main/java/com/fatjoni/droid/myportfolio/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Toast mAppToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,43 +31,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.spotifyStreamer:
-                Toast.makeText(this, "Run Spotify Streamer App", Toast.LENGTH_SHORT).show();
+                //Stop any previous toasts
+                if(mAppToast !=null){
+                    mAppToast.cancel();
+                }
+                mAppToast = Toast.makeText(this, "Run Spotify Streamer App", Toast.LENGTH_SHORT);
+                mAppToast.show();
                 break;
+
             case R.id.scoresApp:
-                Toast.makeText(this,"Run Scores App", Toast.LENGTH_SHORT).show();
+                //Stop any previous toasts
+                if(mAppToast !=null){
+                    mAppToast.cancel();
+                }
+                mAppToast = Toast.makeText(this, "Run Scores App", Toast.LENGTH_SHORT);
+                mAppToast.show();
                 break;
             case R.id.libraryApp:
-                Toast.makeText(this,"Run Library App", Toast.LENGTH_SHORT).show();
+                //Stop any previous toasts
+                if(mAppToast !=null){
+                    mAppToast.cancel();
+                }
+                mAppToast = Toast.makeText(this, "Run Library App", Toast.LENGTH_SHORT);
+                mAppToast.show();
+                break;
             case R.id.buildItBigger:
-                Toast.makeText(this,"Run Build it Bigger App", Toast.LENGTH_SHORT).show();
+                //Stop any previous toasts
+                if(mAppToast !=null){
+                    mAppToast.cancel();
+                }
+                mAppToast = Toast.makeText(this, "Run Build it Bigger App", Toast.LENGTH_SHORT);
+                mAppToast.show();
                 break;
             case R.id.myOwnApp:
-                Toast.makeText(this,"Run My Own App", Toast.LENGTH_SHORT).show();
+                //Stop any previous toasts
+                if(mAppToast !=null){
+                    mAppToast.cancel();
+                }
+                mAppToast = Toast.makeText(this, "Run My Own App", Toast.LENGTH_SHORT);
+                mAppToast.show();
                 break;
         }
     }
